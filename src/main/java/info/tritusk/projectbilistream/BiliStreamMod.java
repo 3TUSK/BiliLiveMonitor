1ec1647073588f11eb87c71f8e48ec6b871bd75b
Epix <git@exz.me>
2016-12-06 20:53:25 +0800

    runnable mod

--- src/main/java/info/tritusk/projectbilistream/BiliStreamMod.java
+++ src/main/java/info/tritusk/projectbilistream/BiliStreamMod.java
@@ -2,23 +2,29 @@ package info.tritusk.projectbilistream;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = BiliStreamMod.MODID, name = BiliStreamMod.NAME, version = "0.0.0.0", clientSideOnly = true)
-public enum BiliStreamMod {
-	
-	INSTANCE;
-	
+public class BiliStreamMod {
+	static final String MODID = "bilistreammod";
+	static final String NAME = "BiliStreamMod";
+
+	@Mod.Instance(MODID)
+	private static BiliStreamMod INSTANCE;
+
 	@Mod.InstanceFactory
 	public BiliStreamMod getInstance() {
 		return INSTANCE;
 	}
-	
+
+	@SideOnly(Side.CLIENT)
 	@Mod.EventHandler
 	public void onLoad(FMLInitializationEvent event) {
 		new PlayerMonitor();
+		Thread t = new Thread(new BiliLiveMonitor());
+		t.start();
 	}
-	
-	public static final String MODID = "bilistreammod";
-	public static final String NAME = "BiliStreamMod";
+
 
 }
