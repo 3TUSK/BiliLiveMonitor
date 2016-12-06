e9870df2d06c0cc5d5874c5d4cd04d6a0194fafe
3TUSK <urey.s.knowledge@gmail.com>
2016-12-06 17:05:16 -0500

    Assume the connection works.
    Ref: http://www.lyyyuna.com/2016/03/14/bilibili-danmu01/

--- src/main/java/info/tritusk/projectbilistream/BiliLiveMonitor.java
+++ src/main/java/info/tritusk/projectbilistream/BiliLiveMonitor.java
@@ -1,24 +1,41 @@
 package info.tritusk.projectbilistream;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
 
 public class BiliLiveMonitor implements Runnable {
 	
 	final Socket biliDanmakuSocket;
 	
 	public BiliLiveMonitor() {
+		// TODO: Consider putting this into separate method(s) instead of stuffing in constructor
+		String danmakuServer = "";
 		try {
-			biliDanmakuSocket = new Socket("http://livecmt.bilibili.com/", 81);
-		} catch (IOException e) {
-			throw new IllegalStateException("BiliLiveMonitor cannot initialize due to error", e);
+			InputStream result = new URL("http://live.bilibili.com/api/player?id=cid:" + ModSettings.liveRoom).openConnection().getInputStream();
+			byte[] bytes = new byte[2048];
+			result.read(bytes);
+			String rawInputAsString = "<root>" + new String(bytes, "UTF-8") + "</root>";
+			InputStream wrappedInput = new ByteArrayInputStream(rawInputAsString.getBytes("UTF-8"));
+			Document parsedResult = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(wrappedInput);
+			danmakuServer = parsedResult.getDocumentElement().getElementsByTagName("server").item(0).getTextContent();
+			biliDanmakuSocket = new Socket(danmakuServer, 81);
+		} catch (Exception e) {
+			throw new RuntimeException("BiliLiveMonitor cannot initialize due to error", e);
 		}
 	}
 
 	@Override
 	public void run() {
 		try {
-			biliDanmakuSocket.getOutputStream();
+			DataOutputStream output = new DataOutputStream(biliDanmakuSocket.getOutputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
